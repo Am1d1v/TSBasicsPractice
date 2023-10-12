981cacc36e3571e5d7e1ced0a8e1d11c6a4a103b
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-12 17:08:43 +0300

    Add files via upload

--- classes.js
+++ classes.js
@@ -3,6 +3,19 @@ class Vehicle {
     drive() {
         console.log('I am driving');
     }
+    startDrawingProcess() {
+        this.drive();
+    }
+    stop() {
+        console.log('Stopped');
+    }
+}
+class Car extends Vehicle {
+    drive() {
+        console.log('Car is driving');
+    }
 }
 const vehicle = new Vehicle();
-vehicle.drive();
+vehicle.startDrawingProcess();
+const someCar = new Car();
+someCar.drive();
--- classes.ts
+++ classes.ts
@@ -4,11 +4,35 @@
 
 class Vehicle {
 
+    protected drive(): void{
+        console.log('I am driving');
+    }
+
+
+    startDrawingProcess(): void{
+        this.drive();
+    }
+
+
+    stop(): void{
+        console.log('Stopped');
+    }
+}
+
+
+class Car extends Vehicle {
+
     drive(): void{
-        console.log('I am driving')
+        console.log('Car is driving')
     }
 }
 
 
 const vehicle = new Vehicle();
-vehicle.drive();
+vehicle.startDrawingProcess();
+
+
+const someCar = new Car();
+someCar.drive();
+
+
