d48d8129020802634d9dcb7f8501ceb05c5a6862
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-12 15:56:01 +0300

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,8 @@
+"use strict";
+class Vehicle {
+    drive() {
+        console.log('I am driving');
+    }
+}
+const vehicle = new Vehicle();
+vehicle.drive();
---
+++ classes.ts
@@ -0,0 +1,14 @@
+
+
+
+
+class Vehicle {
+
+    drive(): void{
+        console.log('I am driving')
+    }
+}
+
+
+const vehicle = new Vehicle();
+vehicle.drive();
